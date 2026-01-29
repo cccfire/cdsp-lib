a813bc1ef7f6d07166f9018809e26f5241196fe2
cccfire <calebcychan@gmail.com>
2026-01-29 12:37:52 -0800

    added standalone target

--- lib/cdsp-app.h
+++ lib/cdsp-app.h
@@ -1,13 +1,17 @@
 #pragma once
+#include <pugl/pugl.h>
+#include <pugl/gl.h>
 
 typedef struct cdsp_app cdsp_app_t;
 typedef struct cdsp_app {
   void *ctx,
+  void *_plugin, // 
   void (*init)(cdsp_app_t*),
 } cdsp_app_t;
 
 typedef struct cdsp_gui cdsp_gui_t;
 typedef struct cdsp_gui {
+  PuglWorld* world,
   void *data
 } cdsp_gui_t;
 
@@ -15,3 +19,4 @@ typedef struct cdsp_dsp cdsp_dsp_t;
 typedef struct cdsp_dsp {
   void *data
 } cdsp_dsp_t;
+
