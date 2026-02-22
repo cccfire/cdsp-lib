c127322eb287552b77527c5d3acd3a10a1006d08
cccfire <calebcychan@gmail.com>
2026-02-22 09:14:28 -0800

    standalone app showing

--- src/main.c
+++ src/main.c
@@ -1,16 +1,20 @@
 #include <stdio.h>
 #include <cdsp/app.h>
+#include <cdsp/gui.h>
 #include "minimal_plugin.c"
 
 int main(int argc, char* argv[]) {
   printf("1\n");
-  fflush(stdout);
   cdsp_app_t* app = calloc(1, sizeof(cdsp_app_t));
   printf("2\n");
-  fflush(stdout);
   create_minimal_app(app);
-  printf("%s\n", app->name);
-  fflush(stdout);
+  cdsp_gui_init(app);
+  cdsp_gui_show(app);
+  while(app->active) {
+    cdsp_gui_update(app);
+  }
+  cdsp_gui_destroy(app);
+  cdsp_destroy_app(app);
   free(app);
   return 1;
 }
